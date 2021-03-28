a100e6d17e4dd08a03077dab1599e2c407bb8856
Serega <65994110+Misnaged@users.noreply.github.com>
2021-03-28 10:59:18 +0300

    Update requests.go

--- requests/requests.go
+++ requests/requests.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"userTA/player"
 	"userTA/responses"
-
-	//"userTA/EventHandler"
 )
 
 //***** Building a transaction request if conditions are passed ****//
@@ -15,14 +13,14 @@ func RequestTA(){
 		switch att {
 		case 1:
 			CallTransAct()
-			fmt.Println(responses.ValidationFlag)
+			fmt.Println("validation flag is", responses.ValidationFlag)
 		case 2:
 			CallTransAct()
-			fmt.Println(responses.ValidationFlag)
+			fmt.Println("validation flag is", responses.ValidationFlag)
 
 		case 3:
 			CallTransAct()
-			fmt.Println(responses.ValidationFlag)
+			fmt.Println("validation flag is", responses.ValidationFlag)
 		}
 		if responses.ValidationFlag == 1{
 			break
@@ -42,55 +40,3 @@ func CallTransAct(){
 	}
 }
 
-/*
-func VerifyAuth() {
-	h1 := func(w http.ResponseWriter, r *http.Request){
-		http.ServeFile(w, r, "user.html")
-	}
-	h2 := func(w http.ResponseWriter, req *http.Request){
-		 l := req.FormValue("username")
-		 p := req.FormValue("passphrase")
-
-
-		loginname := l
-		password := p
-
-		for key, value := range player.Authie {
-			if (loginname == key && password == value){
-				fmt.Println("login and password are accepted")
-				fmt.Fprintf(w, "%s", "\n Accepted")
-			}else{
-				fmt.Println("wrong login/password")
-			}
-		}
-	}
-	http.HandleFunc("/", h1)
-	http.HandleFunc("/postform", h2)
-	fmt.Println("Server is listening..., \n open localhost:8181 on your web browser and fill form")
-	http.ListenAndServe(":8181", nil)
-}
-
-
- */
-
-
-/*
-func FromRequest(req *http.Request) (net.IP, error) {
-
-}
-
-		err:= r.ParseForm() //parse in middleware, data will be contained in r.PostForm
-		if err != nil {
-			log.Fatalf("Failed to decode postFormByteSlice: %v", err)
-		}
-		loginname := r.PostFormValue(l)
-		password := r.PostFormValue(p)
-
-		for key, value := range player.Authie {
-			if (loginname == key && password == value){
-				fmt.Println("zaebok")
-			}else{
-			fmt.Println("xyeta")
-			}
-		}
-*/
