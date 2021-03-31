48a44c23af25221fa9daecc628716910eaa45a3c
Serega <65994110+Misnaged@users.noreply.github.com>
2021-04-01 02:15:25 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,42 @@
 package main
 
 import (
+	"log"
+	"os"
 	"userTA/player"
 	"userTA/requests"
+	"userTA/responses"
+
+	//"userTA/responses"
 )
 var loginStruct player.LoginStruct
 
+
 func main() {
-	player.SetLogin(loginStruct)
-	player.CheckLogin()
-	requests.RequestTA()
+
+	if err:= player.SetLogin(loginStruct); err != nil{
+		log.Println(err)
+		os.Exit(1)
+		return
+	}
+
+	if _, err := player.CheckLogin(loginStruct); err != nil{
+		log.Println(err)
+		os.Exit(1)
+		return
+	}
+
+	if err := requests.RequestTA(); err != nil{
+		log.Println(err)
+		os.Exit(1)
+		return
+	}
+
+
+	if _,_,err := responses.ResponseToTA(); err != nil{
+		log.Println(err)
+		os.Exit(1)
+		return
+	}
+
 }
