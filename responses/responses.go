4a36716f883fac7f5a1e96503f2e0a5a7a2451ea
Serega <65994110+Misnaged@users.noreply.github.com>
2021-04-01 02:12:23 +0300

    Update responses.go

--- responses/responses.go
+++ responses/responses.go
@@ -13,34 +13,23 @@ var ValidTransActions = []int {10000, 20000, 30000}
 //*********** Flags used for managing the code ****//
 var ValidationFlag int
 
+
 func ResponseToTA() (int, int, error){
 	if _, err := fmt.Scan(&UserInput);
 		err != nil {
 		return 0, 0, fmt.Errorf(" Transaction has been failed due to: %w", err)
-      	}
+	}
 
-		for _, ValidTA := range ValidTransActions {
-			areEqual, err := Matching(UserInput, ValidTA)
-
-			if areEqual {fmt.Println("TransAction has been done successfully")}
-
-			if err != nil{
-				return 0, 0, fmt.Errorf(" Transaction is unvalid: %w", err)
-			}
-
-			if ValidTA == UserInput {
-				ValidationFlag = 1
-				break
-			} else {
-				ValidationFlag = 0
-			}
+	for _, ValidTA := range ValidTransActions{
+		if ValidTA == UserInput {
+			fmt.Println("TransAction has been done successfully")
+			fmt.Printf("\n, %d", UserInput)
+			ValidationFlag = 1
+			break
+		} else {
+			ValidationFlag = 0
 		}
+	}
 	return UserInput, ValidationFlag, nil
 }
 
-func Matching(a1, b1  int)  (areEq bool, err error){
-	if a1 == b1{
-		return true, nil
-	}
-	return true, nil
-}
